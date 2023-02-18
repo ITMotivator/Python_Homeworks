0f0a6ce0b9831cb5937c35a4df832166e135e395
Vyacheslav <slava-russia@mail.ru>
2023-02-18 16:31:51 +0700

    Task 1.1 done

--- 1.1.py
+++ 1.1.py
@@ -1,1 +1,7 @@
 # Найдите сумму цифр трехзначного числа.
+number = int(input('Enter 3digit number '))
+ones = number % 10
+tens = (number // 10) % 10
+hundreds = number // 100
+sum = ones + tens + hundreds
+print(f"Number digits sum = {sum}")
