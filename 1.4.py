c1efa1fec2ec9431584d990ae2a614f95d464472
Vyacheslav <slava-russia@mail.ru>
2023-02-18 17:32:44 +0700

    Task 1.4 done

--- 1.4.py
+++ 1.4.py
@@ -1,3 +1,15 @@
 # Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек, 
 # если разрешается сделать один разлом по прямой между дольками (то есть разломить 
 # шоколадку на два прямоугольника).
+
+print("Enter cholcolate size")
+chocoLen = int(input("Length: "))
+chocoWid = int(input("Width: "))
+chocoSquares = int(input("Enter number of squares to cut: "))
+
+if chocoSquares >= chocoLen * chocoWid:
+    print("Error!")
+elif chocoSquares % chocoLen == 0 or chocoSquares % chocoWid == 0:
+    print("Yes")
+else:
+    print("No")
