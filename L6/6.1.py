d42d4389720a81dc0642326a073e4c22db658f6d
Vyacheslav <slava-russia@mail.ru>
2023-03-08 14:46:18 +0700

    Task 6.1 done

--- L6/6.1.py
+++ L6/6.1.py
@@ -3,5 +3,14 @@
 # n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
 
-str = input("Enter progression 1st elem, step & length by spaces ")
-print(str)
+firstEl = int(input("Enter progression 1st elem "))
+step = int(input("Enter progression step "))
+length = int(input("Enter progression length "))
+progressionList = []
+
+current = firstEl
+for i in range(1, length + 1):
+    current = firstEl + (i - 1) * step
+    progressionList.append(current)
+     
+print(progressionList)
---
+++ test.py
@@ -0,0 +1,2 @@
+str = input("Enter progression 1st elem, step & length by spaces ")
+print(str)
